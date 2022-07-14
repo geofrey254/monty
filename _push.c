9c11371fdd888744cabf6418a708015e202c49b1
geofrey254 <devongeoffreymaina@gmail.com>
2022-07-14 11:05:38 -0700

    pall function added

--- _push.c
+++ _push.c
@@ -1,5 +1,8 @@
 #include "monty.h"
 
+void monty_push(stack_t **stack, unsigned int line_number);
+void monty_pall(stack_t **stack, unsigned int line_number);
+
 /**
  * monty_push - Pushes a value to a stack_t linked list.
  * @stack: Pointer to the top mode node of a stack_t linked list.
@@ -54,4 +57,21 @@ void monty_push(stack_t **stack, unsigned int line_number)
 		new->next = NULL;
 		tmp->next = new;
 	}
+}
+
+/**
+ * monty_pall - Prints the values of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+	(void)line_number;
 }
